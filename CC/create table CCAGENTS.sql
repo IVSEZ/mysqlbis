5d6d3b69a73bc62a38d273e6b1d983198ab70528
rahulw-iv <rahulw.iv@gmail.com>
2018-05-26 13:11:30 +0400

    raw

---
+++ CC/create table CCAGENTS.sql
@@ -0,0 +1,12 @@
+use rcbill_my;
+
+drop table if exists ccagents;
+
+CREATE TABLE `ccagents` (
+`CCDATE` datetime DEFAULT NULL ,
+`CCSHIFT` varchar(255) DEFAULT NULL ,
+`CALLAGENT` varchar(255) DEFAULT NULL ,
+`CCAGENTNAME` varchar(255) DEFAULT NULL ,
+`INSERTEDON` datetime DEFAULT NULL 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
