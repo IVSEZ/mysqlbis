4f37371b8ee74154a08e0eafce11d8a4657ad2c8
rahulw-iv <rahulw.iv@gmail.com>
2018-11-21 08:17:42 +0400

    raw

--- Analysis/PAYMENT_COLLECTIONS_REPORT.sql
+++ Analysis/PAYMENT_COLLECTIONS_REPORT.sql
@@ -2,8 +2,10 @@ set @startdate='2018-01-01';
 set @enddate='2018-10-31';
 set @startdate='2017-01-01';
 set @enddate='2017-12-31';
+set @startdate='2018-10-01';
+set @enddate='2018-10-31';
 
-
+/*
 select * 
 , paydate, week(PAYDATE,1) as weeknum, rcbill_my.GetWeekdayName(weekday(paydate)) as weekday, money, (select name from rcbill.rcb_payobjects where id=payobjectid) as PayObjectName
 , (select name from rcbill.rcb_services where id=(paytype*-1)) as PayTypeName 
@@ -12,17 +14,18 @@ from rcbill.rcb_casa 	where
 	(hard not in (100, 101, 102) or hard is null)
     and date(PAYDATE)>=@startdate and date(PAYDATE)<=@enddate;
 
-
-
-
-
 select week('2018-01-13 00:00:00',1), weekday('2018-01-13 00:00:00');
+*/
 
+## REPORT FOR PAYMENT COLLECTIONS
 select paydate, week(PAYDATE,1) as weeknum, rcbill_my.GetWeekdayName(weekday(paydate)) as weekday, money
 , (select name from rcbill.rcb_payobjects where id=payobjectid) as PayObjectName
 , (select name from rcbill.rcb_services where id=(paytype*-1)) as PayTypeName 
 , (select name from rcbill.rcb_cashpoints where id=cashpointid) as CashPointName
-from rcbill.rcb_casa 
+from 
+ rcbill.rcb_casa 
+	-- only the residential customers
+    -- (select * from rcbill.rcb_casa where CLID in (select id from rcbill.rcb_tclients where CLClass=13)) tx
 	where
 	(hard not in (100, 101, 102) or hard is null)
    and date(PAYDATE)>=@startdate and date(PAYDATE)<=@enddate
@@ -30,21 +33,32 @@ from rcbill.rcb_casa
     ;
 
 
+
+-- CHANNEL
 select date(PAYDATE) as paydate,day(PAYDATE) as payday, month(PAYDATE) as paymonth, year(PAYDATE) as payyear, weeknum, weekday, payobjectname as pay_channel, sum(money) as pay_amount
 from 
 (
 	select paydate, week(PAYDATE,1) as weeknum, rcbill_my.GetWeekdayName(weekday(paydate)) as weekday, money, (select name from rcbill.rcb_payobjects where id=payobjectid) as PayObjectName
 	, (select name from rcbill.rcb_services where id=(paytype*-1)) as PayTypeName 
 	, (select name from rcbill.rcb_cashpoints where id=cashpointid) as CashPointName
-	from rcbill.rcb_casa 
-	where
+	from 
+    rcbill.rcb_casa 
+	-- only the residential customers
+    -- (select * from rcbill.rcb_casa where CLID in (select id from rcbill.rcb_tclients where CLClass=13)) tx
+    where
 	(hard not in (100, 101, 102) or hard is null)
-	-- and date(PAYDATE)>=@startdate and date(PAYDATE)<=@enddate
+	and date(PAYDATE)>=@startdate and date(PAYDATE)<=@enddate
 	-- limit 100
 ) a 
+-- where payobjectname = 'CHEQUE'
 group by 1,2,3,4,5,6,7
+-- with rollup
 ; 
 
+
+
+
+-- POS
 select date(PAYDATE) as paydate, day(PAYDATE) as payday, month(PAYDATE) as paymonth, year(PAYDATE) as payyear, weeknum, weekday, cashpointname as pay_pos, sum(money) as pay_amount
 from 
 (
@@ -68,3 +82,5 @@ select sum(money) from rcbill.rcb_casa
     and date(PAYDATE)>='2018-01-01' and date(PAYDATE)<='2018-10-31'
     -- limit 100
     ;
+
+select * from rcbill.rcb_casa limit 100;
