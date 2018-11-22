dcb12ff7a45669002af9d5aea5ae139e07f8d461
bisuser <intelvision.sc@outlook.com>
2018-11-22 08:43:34 +0400

    data-bis

--- _DailyScript/_indexfile.sql
+++ _DailyScript/_indexfile.sql
@@ -9,7 +9,7 @@ mysql \T C:\workspace\cloud\code\sql\_log\output_20180712.out
 
 
 mysql -h 192.168.1.166 -u root -pl3tm31n!@#mysql 
-mysql \T C:\workspace\cloud\code\sql\_log\output_20181121_1.out
+mysql \T C:\workspace\cloud\code\sql\_log\output_20181122_1.out
 
 mysql \. C:\workspace\cloud\code\sql\_DailyScript\_setdates.sql
 
@@ -60,7 +60,7 @@ use rcbill_my;
 
 call sp_ActiveNumber(31,10,2018,'','');
 
-call sp_GetActiveNumberFromTo('2018-11-16','2018-11-18');
+call sp_GetActiveNumberFromTo('2018-11-19','2018-11-20');
 
 
 select * from rcbill_my.clientstats where VOD>0 and DualView>0
