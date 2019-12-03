49222f092c2a689e9afa893071fd16a5d1981f39
rahulw-iv <rahulw.iv@gmail.com>
2019-12-03 14:26:48 +0400

    raw

--- Analysis/HFC_GPON_ACTIVECUSTOMERS.sql
+++ Analysis/HFC_GPON_ACTIVECUSTOMERS.sql
@@ -4,7 +4,7 @@ clean_connection_type
 , clean_mxk_name , clean_hfc_nodename 
 , count(clientcode) as clients
 from rcbill_my.rep_custconsolidated
-where clean_mxk_name like '%BEAU%' or clean_hfc_nodename like '%OMBRE%' or clean_hfc_nodename like '%BEAU%'
+-- where clean_mxk_name like '%BEAU%' or clean_hfc_nodename like '%OMBRE%' or clean_hfc_nodename like '%BEAU%'
 
 group by 1, 2, 3 , 4, 5
 
