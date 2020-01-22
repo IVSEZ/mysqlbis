bd68f6c5a8c4f2273b583aee904f3c8b88aa5987
bisuser <intelvision.sc@outlook.com>
2020-01-22 12:43:15 +0400

    data-bis

--- _DailyScript/_setdates.sql
+++ _DailyScript/_setdates.sql
@@ -2,27 +2,27 @@
 use rcbill;
 
 #SET DATE
-SET @REPORTDATE=str_to_date('2020-01-20','%Y-%m-%d');
+SET @REPORTDATE=str_to_date('2020-01-21','%Y-%m-%d');
 
-SET @rundate='2020-01-20';
+SET @rundate='2020-01-21';
 
 SET @COLNAME1='CLIENTDEBT_REPORTDATE';
 
-set @periodstart='2020-01-20';
-set @periodend='2020-01-20';
+set @periodstart='2020-01-21';
+set @periodend='2020-01-21';
 
 
-SET @rundate='2020-01-20';
+SET @rundate='2020-01-21';
 
 #FOR Daily calls script
-SET @date1='2020-01-20';
-SET @date2='2020-01-21';
+SET @date1='2020-01-21';
+SET @date2='2020-01-22';
 
 
 ## FOR BUDGET ACTUAL ANALYSIS SCRIPT
 ## change the @m# date for the relevant date
 set @revenue=1;
-set @m1='2020-01-20';
+set @m1='2020-01-21';
 
 set @m2='2020-02-28';
 set @m3='2020-03-31';
