5f79a89ce3d9b2373db4050a9d07f0e2059c1325
rahulw-iv <rahulw.iv@gmail.com>
2020-03-28 07:55:32 +0400

    raw

--- Analysis/voucher_analysis_1.sql
+++ Analysis/voucher_analysis_1.sql
@@ -7,12 +7,59 @@ and (trim(upper(comment)) REGEXP 'VOUCHER')
 order by commentdate desc
 ;
 
-select clientcode, clientname, contractcode, month(OPENDATE) as OPENMONTH, year(OPENDATE) as OPENYEAR, count(distinct ticketid) as d_tickets, count(distinct clientcode) as d_clients  from  rcbill_my.clientticketjourney 
+select clientcode, clientname, contractcode, month(OPENDATE) as OPENMONTH, year(OPENDATE) as OPENYEAR,  ticketid, commentuser, `comment` 
+from  rcbill_my.clientticketjourney a
+where 0=0 
+-- and commentuser in ('Rahul Walavalkar') 
+and ticketid in 
+(
+select ticketid from  rcbill_my.clientticketjourney 
+where 0=0 
+-- and commentuser in ('Rahul Walavalkar') 
+and (trim(upper(comment)) REGEXP 'VOUCHER')
+)
+
+group by 1,2,3,4,5, 6
+order by commentdate asc
+;
+
+select * from rcbill.rcb_casa where CLID in (select id from rcbill.rcb_tclients where kod='I.000005861');
+
+
+select clientcode, clientname, contractcode, month(OPENDATE) as OPENMONTH, year(OPENDATE) as OPENYEAR,  count(distinct ticketid) as d_tickets from  rcbill_my.clientticketjourney 
 where 0=0 
 -- and commentuser in ('Rahul Walavalkar') 
 and (trim(upper(comment)) REGEXP 'VOUCHER')
 
 group by 1,2,3,4,5
-order by 7 desc
-
+order by 6 desc
 ;
+
+select clientcode, clientname, contractcode, month(OPENDATE) as OPENMONTH, year(OPENDATE) as OPENYEAR, count(distinct ticketid) as d_tickets from  rcbill_my.clientticketjourney 
+where 0=0 
+-- and commentuser in ('Rahul Walavalkar') 
+and (trim(upper(comment)) REGEXP 'VOUCHER')
+
+group by 1,2,3,4,5
+order by 6 desc
+;
+
+select clientcode, clientname, contractcode, year(OPENDATE) as OPENYEAR, count(distinct ticketid) as d_tickets from  rcbill_my.clientticketjourney 
+where 0=0 
+-- and commentuser in ('Rahul Walavalkar') 
+and (trim(upper(comment)) REGEXP 'VOUCHER')
+
+group by 1,2,3,4
+order by 5 desc
+;
+
+select clientcode, clientname, year(OPENDATE) as OPENYEAR, count(distinct ticketid) as d_tickets from  rcbill_my.clientticketjourney 
+where 0=0 
+-- and commentuser in ('Rahul Walavalkar') 
+and (trim(upper(comment)) REGEXP 'VOUCHER')
+
+group by 1,2,3
+order by 4 desc
+;
+
+select * from rcbill_my.customers_contracts_collection_pivot where clientcode ='I.000005861';
