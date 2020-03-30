6c050c3a732f5b3cebcab6072fb37cd076dd7b46
bisuser <intelvision.sc@outlook.com>
2020-03-30 09:40:34 +0400

    data-bis

--- Analysis/voucher_analysis_1.sql
+++ Analysis/voucher_analysis_1.sql
@@ -7,20 +7,31 @@ and (trim(upper(comment)) REGEXP 'VOUCHER')
 order by commentdate desc
 ;
 
-select clientcode, clientname, contractcode, month(OPENDATE) as OPENMONTH, year(OPENDATE) as OPENYEAR,  ticketid, commentuser, `comment` 
-from  rcbill_my.clientticketjourney a
-where 0=0 
--- and commentuser in ('Rahul Walavalkar') 
-and ticketid in 
+select a.*
+-- , b.*
+, b.IsAccountActive, b.AccountActivityStage, b.activenetwork, b.activeservices, b.clientclass, b.clientaddress, b.clientarea
+-- , b.TotalPayments2020, b.TotalPaymentAmount2020, b.TotalPayments2019, b.TotalPaymentAmount2019
+from 
 (
-select ticketid from  rcbill_my.clientticketjourney 
-where 0=0 
--- and commentuser in ('Rahul Walavalkar') 
-and (trim(upper(comment)) REGEXP 'VOUCHER')
-)
 
-group by 1,2,3,4,5, 6
-order by commentdate asc
+	select clientcode, clientname, contractcode, month(OPENDATE) as OPENMONTH, year(OPENDATE) as OPENYEAR,  ticketid, commentuser, commentdate, `comment` 
+	from  rcbill_my.clientticketjourney a
+	where 0=0 
+	-- and commentuser in ('Rahul Walavalkar') 
+	and ticketid in 
+	(
+	select ticketid from  rcbill_my.clientticketjourney 
+	where 0=0 
+	-- and commentuser in ('Rahul Walavalkar') 
+	and (trim(upper(comment)) REGEXP 'VOUCHER')
+	)
+	and year(OPENDATE)>2018
+	group by 1,2,3,4,5, 6
+	order by commentdate desc
+) a 
+inner join 
+rcbill_my.rep_custconsolidated b
+on a.clientcode=b.clientcode
 ;
 
 select * from rcbill.rcb_casa where CLID in (select id from rcbill.rcb_tclients where kod='I.000005861') order by ID desc;
@@ -33,7 +44,6 @@ select clientcode, clientname, contractcode, month(OPENDATE) as OPENMONTH, year(
 where 0=0 
 -- and commentuser in ('Rahul Walavalkar') 
 and (trim(upper(comment)) REGEXP 'VOUCHER')
-
 group by 1,2,3,4,5
 order by 6 desc
 ;
