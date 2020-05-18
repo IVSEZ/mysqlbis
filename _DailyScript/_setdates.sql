2f1fb1a76dea1cff328445cfdb15743bd41f3748
bisuser <intelvision.sc@outlook.com>
2020-05-18 16:14:50 +0400

    data-bis

--- _DailyScript/_setdates.sql
+++ _DailyScript/_setdates.sql
@@ -2,21 +2,21 @@
 use rcbill;
 
 #SET DATE
-SET @REPORTDATE=str_to_date('2020-05-16','%Y-%m-%d');
+SET @REPORTDATE=str_to_date('2020-05-17','%Y-%m-%d');
 
-SET @rundate='2020-05-16';
+SET @rundate='2020-05-17';
 
 SET @COLNAME1='CLIENTDEBT_REPORTDATE';
 
-set @periodstart='2020-05-16';
-set @periodend='2020-05-16';
+set @periodstart='2020-05-17';
+set @periodend='2020-05-17';
 
 
-SET @rundate='2020-05-16';
+SET @rundate='2020-05-17';
 
 #FOR Daily calls script
-SET @date1='2020-05-16';
-SET @date2='2020-05-17';
+SET @date1='2020-05-17';
+SET @date2='2020-05-18';
 
 
 ## FOR BUDGET ACTUAL ANALYSIS SCRIPT
@@ -27,7 +27,7 @@ set @m2='2020-02-29';
 set @m3='2020-03-31';
 set @m4='2020-04-30';
 
-set @m5='2020-05-16';
+set @m5='2020-05-17';
 
 set @m6='2020-06-30';
 set @m7='2020-07-31';
