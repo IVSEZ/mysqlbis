790a094232b065158f1da5b0b7e0b564fcbca6ea
bisuser <intelvision.sc@outlook.com>
2021-01-02 10:34:20 +0400

    data-bis

--- _DailyScript/REPORT_LIST.sql
+++ _DailyScript/REPORT_LIST.sql
@@ -156,7 +156,7 @@ select * from rcbill_my.rep_activenumberavg3 where lastday='2020-06-30';
 select * from rcbill_my.rep_activenumberavg3;
 ## MONTH ACTIVE NUMBER REPORT
 use rcbill_my;
-call sp_GetActiveNumberFromTo('2020-12-11','2020-12-23');
+call sp_GetActiveNumberFromTo('2020-12-01','2020-12-31');
 
 ## BUDGET VS ACTUAL ANALYSIS
 select * from rcbill_my.rep_budget_actual_2019_pv;
--- _DailyScript/_setdates.sql
+++ _DailyScript/_setdates.sql
@@ -2,36 +2,36 @@
 use rcbill;
 
 #SET DATE
-SET @REPORTDATE=str_to_date('2020-12-30','%Y-%m-%d');
+SET @REPORTDATE=str_to_date('2021-01-01','%Y-%m-%d');
 
-SET @rundate='2020-12-30';
+SET @rundate='2021-01-01';
 
 SET @COLNAME1='CLIENTDEBT_REPORTDATE';
 
-set @periodstart='2020-12-30';
-set @periodend='2020-12-30';
+set @periodstart='2021-01-01';
+set @periodend='2021-01-01';
 
 
-SET @rundate='2020-12-30';
+SET @rundate='2021-01-01';
 
 #FOR Daily calls script
-SET @date1='2020-12-30';
-SET @date2='2020-12-31';
+SET @date1='2021-01-01';
+SET @date2='2021-01-02';
 
 
 ## FOR BUDGET ACTUAL ANALYSIS SCRIPT
 ## change the @m# date for the relevant date
 set @revenue=1;
-set @m1='2020-01-31';
-set @m2='2020-02-29';
-set @m3='2020-03-31';
-set @m4='2020-04-30';
-set @m5='2020-05-31';
-set @m6='2020-06-30';
-set @m7='2020-07-31';
-set @m8='2020-08-31';
-set @m9='2020-09-30';
-set @m10='2020-10-31';
-set @m11='2020-11-30';
+set @m1='2021-01-01';
 
-set @m12='2020-12-30';
+set @m2='2021-02-28';
+set @m3='2021-03-31';
+set @m4='2021-04-30';
+set @m5='2021-05-31';
+set @m6='2021-06-30';
+set @m7='2021-07-31';
+set @m8='2021-08-31';
+set @m9='2021-09-30';
+set @m10='2021-10-31';
+set @m11='2021-11-30';
+set @m12='2021-12-31';
