487cce66bb713cb652b20a1a821078abcee92389
rahulw-iv <rahulw.iv@gmail.com>
2021-01-21 15:29:48 +0400

    raw

--- data_extract/data_extract_issues-1.sql
+++ data_extract/data_extract_issues-1.sql
@@ -28,3 +28,21 @@ select * from rcbill.rcb_invoicescontents where INVOICENO=@ddno;
 
 select * from rcbill.rcb_invoicesheader where year(BEGDATE)=3000;
 select * from rcbill.rcb_invoicescontents where INVOICENO in (select INVOICENO from rcbill.rcb_invoicesheader where year(BEGDATE)=3000);
+
+select * from rcbill_extract.IV_SERVICEINSTANCE where CUSTOMERACCOUNTNUMBER in ('CA_I.000000082');
+
+
+select * from rcbill_extract.IV_SERVICEINSTANCE where PACKAGENAME in ('STANDARD');
+
+select * from rcbill_extract.IV_SERVICEINSTANCE where PACKAGENAME in ('CORPORATE POSTPAID');
+
+select * from rcbill_extract.IV_INVENTORY where INVENTORYNUMBER='cc.d3.e2.8d.be.23';
+
+select * from rcbill_extract.IV_SERVICEINSTANCE where SERVICEINSTANCENUMBER='SIN_4180297_8713';
+
+select * from rcbill_extract.IV_PREP_SERVICEINSTANCE1 where clientcode='I9661'; 
+
+
+select * from rcbill.clientcontractsservicepackageprice where clientcode in ('I9661');
+select * from rcbill_my.rep_clientcontractdevices where CLIENT_CODE in ('I9661');
+select * from rcbill_extract.IV_PREP_clientcontractsservicepackagepricedevice where clientcode in ('I9661') order by contractenddate desc;
