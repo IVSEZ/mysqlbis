600788d83f1d5bdf8f77fcde3ae9fed7f9992414
bisuser <intelvision.sc@outlook.com>
2021-01-28 12:34:11 +0400

    data-bis

--- Analysis/PRASLIN_customers.sql
+++ Analysis/PRASLIN_customers.sql
@@ -9,7 +9,27 @@ reportdate, clientarea, clientcode, currentdebt, IsAccountActive, AccountActivit
 
 from rcbill_my.rep_custconsolidated
 where (clientarea in ('PRASLIN') or clientaddress like '%PRASLIN%')
-and IsAccountActive='InActive'
+-- and IsAccountActive='InActive'
+
+;
+
+
+select 
+reportdate, clientarea, clientcode, currentdebt, IsAccountActive, AccountActivityStage
+, clientname, clientclass, clientaddress
+, clientemail, clientnin, clientphone
+, clean_mxk_name, clean_mxk_interface
+, firstactivedate, lastactivedate, dayssincelastactive, firstcontractdate, firstpaymentdate, lastpaymentdate
+, lastpaidamount, totalpaymentamount, TotalPaymentAmount2019, AvgMonthlyPayment2019, TotalPaymentAmount2018, AvgMonthlyPayment2018
+
+
+from rcbill_my.rep_custconsolidated
+where clientcode in 
+(
+	select clientcode from rcbill_my.clientstats
+	where region='PRASLIN' and 
+	(`services` = 'All'  or `services`  like '%Voice%')
+)
 ;
 
 
@@ -22,9 +42,21 @@ select clientarea, count(clientcode) as clients from rcbill_my.rep_custconsolida
 group by clientarea
 ;
 
+select * from rcbill_my.rep_custconsolidated where clientarea='Praslin';
 
-select * from rcbill_my.rep_custconsolidated
-where clientarea='SILHOUETTE ISLAND'
+select * from rcbill_my.clientstats
+where region='PRASLIN' and 
+(`Intel Voice 10`>0 or `Intel Voice 20`>0 or `Voice Plus`>0)
 ;
+
+
+
+select * from rcbill_my.clientstats
+where region='PRASLIN' and 
+(`services` = 'All'  or `services`  like '%Voice%')
+;
+
+
+select * from rcbill_my.clientstats where region='Praslin';
 
 */
--- _DailyScript/_setdates.sql
+++ _DailyScript/_setdates.sql
@@ -2,27 +2,27 @@
 use rcbill;
 
 #SET DATE
-SET @REPORTDATE=str_to_date('2021-01-24','%Y-%m-%d');
+SET @REPORTDATE=str_to_date('2021-01-27','%Y-%m-%d');
 
-SET @rundate='2021-01-24';
+SET @rundate='2021-01-27';
 
 SET @COLNAME1='CLIENTDEBT_REPORTDATE';
 
-set @periodstart='2021-01-24';
-set @periodend='2021-01-24';
+set @periodstart='2021-01-27';
+set @periodend='2021-01-27';
 
 
-SET @rundate='2021-01-24';
+SET @rundate='2021-01-27';
 
 #FOR Daily calls script
-SET @date1='2021-01-24';
-SET @date2='2021-01-25';
+SET @date1='2021-01-27';
+SET @date2='2021-01-28';
 
 
 ## FOR BUDGET ACTUAL ANALYSIS SCRIPT
 ## change the @m# date for the relevant date
 set @revenue=1;
-set @m1='2021-01-24';
+set @m1='2021-01-27';
 
 set @m2='2021-02-28';
 set @m3='2021-03-31';
