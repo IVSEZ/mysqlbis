540ef07b05e4a45c0e6bbcc251b6fc3cf93d1bae
bisuser <intelvision.sc@outlook.com>
2021-02-09 13:43:25 +0400

    data-bis

--- Analysis/report_lynsey_1.sql
+++ Analysis/report_lynsey_1.sql
@@ -5,11 +5,16 @@ use rcbill_my;
 -- select * from rcbill_my.rep_custconsolidated;
 -- clientclass, clienttype, services, ActiveCount, contractcount, period, clientcode, clientname, region, network, 10GB, 20GB, 40GB, Amber, Amber Corporate, Basic, Business Unlimited-1, Business Unlimited-2, Business Unlimited-6, Business Unlimited-8, Business Unlimited-8-daytime, Crimson, Crimson Corporate, Dedicated Custom, Dedicated Plus, DualView, MultiView, VOD, IGO, Mobile Indian, Elite, Executive, Extravagance, Extravagance Corporate, Extreme, Extreme Plus, French, Hotels/Channels, Hotels/Decoder, Indian, Indian Corporate, Intel Data 10, Intel Voice 10, Intel Voice 20, Intelenovela, PBX, Performance, Performance Plus, Prepaid, Prepaid Data, Prestige, Starter, Turquoise High Tide, Turquoise Low Tide, TurquoiseTV, Value, Voice Plus, VPN
 
+##################################
+-- current VOD customers
 
- -- Number of customers on Elite, Extreme, Extreme+, Crimson + Any TV package (excluding indian and french)
- 
+select 'current VOD customers' as message;
 select 
-clientclass, clienttype, services, ActiveCount, contractcount, period, clientcode, clientname, region, network
+a.clientclass, a.clienttype, a.services, a.ActiveCount, a.contractcount, a.period, a.clientcode, a.clientname, a.region, a.network
+, b.TotalPaymentAmount2021, b.AvgMonthlyPayment2021
+, b.TotalPaymentAmount2020, b.AvgMonthlyPayment2020
+, b.TotalPaymentAmount2019, b.AvgMonthlyPayment2019
+, b.TotalPaymentAmount2018, b.AvgMonthlyPayment2018
 , `Amber`, `Amber Corporate`
 , `Crimson`, `Crimson Corporate`
 , `Intel Data 10`
@@ -32,7 +37,58 @@ clientclass, clienttype, services, ActiveCount, contractcount, period, clientcod
 , `Hotels/Channels`, `Hotels/Decoder`
 , `VPN`
 
-from rcbill_my.clientstats
+from rcbill_my.clientstats a
+
+inner join 
+
+rcbill_my.rep_custconsolidated b
+on a.clientcode=b.clientcode
+
+where 
+`VOD`>0
+-- and clientclass not in ('Intelvision Office','Employee')
+;
+
+
+
+##################################
+-- Number of customers on Elite, Extreme, Extreme+, Crimson + Any TV package (excluding indian and french)
+select 'customers on Elite, Extreme, Extreme+, Crimson + Any TV package (excluding indian and french)' as message;
+select 
+a.clientclass, a.clienttype, a.services, a.ActiveCount, a.contractcount, a.period, a.clientcode, a.clientname, a.region, a.network
+, b.TotalPaymentAmount2021, b.AvgMonthlyPayment2021
+, b.TotalPaymentAmount2020, b.AvgMonthlyPayment2020
+, b.TotalPaymentAmount2019, b.AvgMonthlyPayment2019
+, b.TotalPaymentAmount2018, b.AvgMonthlyPayment2018
+, `Amber`, `Amber Corporate`
+, `Crimson`, `Crimson Corporate`
+, `Intel Data 10`
+, `Starter`, `Value`
+, `Elite`, `Extreme`, `Extreme Plus`
+, `Performance`, `Performance Plus`
+, `Basic`, `Executive`, `Extravagance`, `Extravagance Corporate`
+, `French`, `Indian`, `Indian Corporate`
+, `Intelenovela`
+, `DualView`, `MultiView`, `VOD`, `IGO`, `Mobile Indian`
+, `Turquoise High Tide`, `Turquoise Low Tide`, `TurquoiseTV`
+, `Intel Voice 10`, `Intel Voice 20`
+, `Voice Plus`
+
+, `PBX`
+, `Prepaid`
+, `Prepaid Data`
+, `Business Unlimited-1`, `Business Unlimited-2`, `Business Unlimited-6`, `Business Unlimited-8`, `Business Unlimited-8-daytime`
+, `Dedicated Custom`, `Dedicated Plus`
+, `Hotels/Channels`, `Hotels/Decoder`
+, `VPN`
+
+from rcbill_my.clientstats a
+
+inner join 
+
+rcbill_my.rep_custconsolidated b
+on a.clientcode=b.clientcode
+
 where 
 (`Elite`>0 or `Extreme`>0 or `Extreme Plus`>0 or `Crimson`>0 or `Crimson Corporate`>0) 
 and 
@@ -40,9 +96,99 @@ and
 ;
 
 
--- Number of customers on Performance, Performance+ & Amber + Basic or Executive
+-- Number of customers on Elite, Extreme, Extreme+, Crimson + (Basic or Executive)
+select 'customers on Elite, Extreme, Extreme+, Crimson + (Basic or Executive)' as message;
 select 
-clientclass, clienttype, services, ActiveCount, contractcount, period, clientcode, clientname, region, network
+a.clientclass, a.clienttype, a.services, a.ActiveCount, a.contractcount, a.period, a.clientcode, a.clientname, a.region, a.network
+, b.TotalPaymentAmount2021, b.AvgMonthlyPayment2021
+, b.TotalPaymentAmount2020, b.AvgMonthlyPayment2020
+, b.TotalPaymentAmount2019, b.AvgMonthlyPayment2019
+, b.TotalPaymentAmount2018, b.AvgMonthlyPayment2018
+, `Amber`, `Amber Corporate`
+, `Crimson`, `Crimson Corporate`
+, `Intel Data 10`
+, `Starter`, `Value`
+, `Elite`, `Extreme`, `Extreme Plus`
+, `Performance`, `Performance Plus`
+, `Basic`, `Executive`, `Extravagance`, `Extravagance Corporate`
+, `French`, `Indian`, `Indian Corporate`
+, `Intelenovela`
+, `DualView`, `MultiView`, `VOD`, `IGO`, `Mobile Indian`
+, `Turquoise High Tide`, `Turquoise Low Tide`, `TurquoiseTV`
+, `Intel Voice 10`, `Intel Voice 20`
+, `Voice Plus`
+
+, `PBX`
+, `Prepaid`
+, `Prepaid Data`
+, `Business Unlimited-1`, `Business Unlimited-2`, `Business Unlimited-6`, `Business Unlimited-8`, `Business Unlimited-8-daytime`
+, `Dedicated Custom`, `Dedicated Plus`
+, `Hotels/Channels`, `Hotels/Decoder`
+, `VPN`
+
+from rcbill_my.clientstats a
+inner join 
+rcbill_my.rep_custconsolidated b
+on a.clientcode=b.clientcode
+
+where 
+(`Elite`>0 or `Extreme`>0 or `Extreme Plus`>0 or `Crimson`>0 or `Crimson Corporate`>0) 
+and 
+(`Basic`>0 or `Executive`>0) -- or `Extravagance`>0 or `Extravagance Corporate`>0)
+;
+
+-- Number of customers on Elite, Extreme, Extreme+, Crimson + (Extravagance)
+select 'customers on Elite, Extreme, Extreme+, Crimson + (Extravagance)' as message;
+select 
+a.clientclass, a.clienttype, a.services, a.ActiveCount, a.contractcount, a.period, a.clientcode, a.clientname, a.region, a.network
+, b.TotalPaymentAmount2021, b.AvgMonthlyPayment2021
+, b.TotalPaymentAmount2020, b.AvgMonthlyPayment2020
+, b.TotalPaymentAmount2019, b.AvgMonthlyPayment2019
+, b.TotalPaymentAmount2018, b.AvgMonthlyPayment2018
+, `Amber`, `Amber Corporate`
+, `Crimson`, `Crimson Corporate`
+, `Intel Data 10`
+, `Starter`, `Value`
+, `Elite`, `Extreme`, `Extreme Plus`
+, `Performance`, `Performance Plus`
+, `Basic`, `Executive`, `Extravagance`, `Extravagance Corporate`
+, `French`, `Indian`, `Indian Corporate`
+, `Intelenovela`
+, `DualView`, `MultiView`, `VOD`, `IGO`, `Mobile Indian`
+, `Turquoise High Tide`, `Turquoise Low Tide`, `TurquoiseTV`
+, `Intel Voice 10`, `Intel Voice 20`
+, `Voice Plus`
+
+, `PBX`
+, `Prepaid`
+, `Prepaid Data`
+, `Business Unlimited-1`, `Business Unlimited-2`, `Business Unlimited-6`, `Business Unlimited-8`, `Business Unlimited-8-daytime`
+, `Dedicated Custom`, `Dedicated Plus`
+, `Hotels/Channels`, `Hotels/Decoder`
+, `VPN`
+
+from rcbill_my.clientstats a
+inner join 
+rcbill_my.rep_custconsolidated b
+on a.clientcode=b.clientcode
+
+where 
+(`Elite`>0 or `Extreme`>0 or `Extreme Plus`>0 or `Crimson`>0 or `Crimson Corporate`>0) 
+and 
+-- (`Basic`>0 or `Executive`>0 or
+ (`Extravagance`>0 or `Extravagance Corporate`>0)
+;
+
+
+##################################
+-- Number of customers on Performance, Performance+ & Amber + Basic or Executive
+select 'customers on Performance, Performance+ & Amber + Basic or Executive' as message;
+select 
+a.clientclass, a.clienttype, a.services, a.ActiveCount, a.contractcount, a.period, a.clientcode, a.clientname, a.region, a.network
+, b.TotalPaymentAmount2021, b.AvgMonthlyPayment2021
+, b.TotalPaymentAmount2020, b.AvgMonthlyPayment2020
+, b.TotalPaymentAmount2019, b.AvgMonthlyPayment2019
+, b.TotalPaymentAmount2018, b.AvgMonthlyPayment2018
 , `Amber`, `Amber Corporate`
 , `Crimson`, `Crimson Corporate`
 , `Intel Data 10`
@@ -62,16 +208,27 @@ clientclass, clienttype, services, ActiveCount, contractcount, period, clientcod
 , `Dedicated Custom`, `Dedicated Plus`
 , `Hotels/Channels`, `Hotels/Decoder`
 , `VPN`
-from rcbill_my.clientstats
+
+from rcbill_my.clientstats a
+inner join 
+rcbill_my.rep_custconsolidated b
+on a.clientcode=b.clientcode
+
 where 
 (`Performance`>0 or `Performance Plus`>0 or `Amber`>0 or `Amber Corporate`>0) 
 and 
 (`Basic`>0 or `Executive`>0)
 ;
 
+##################################
 -- Number of customers on Performance, Performance+ & Amber + Extravagance
+select 'customers on Performance, Performance+ & Amber + Extravagance' as message;
 select 
-clientclass, clienttype, services, ActiveCount, contractcount, period, clientcode, clientname, region, network
+a.clientclass, a.clienttype, a.services, a.ActiveCount, a.contractcount, a.period, a.clientcode, a.clientname, a.region, a.network
+, b.TotalPaymentAmount2021, b.AvgMonthlyPayment2021
+, b.TotalPaymentAmount2020, b.AvgMonthlyPayment2020
+, b.TotalPaymentAmount2019, b.AvgMonthlyPayment2019
+, b.TotalPaymentAmount2018, b.AvgMonthlyPayment2018
 , `Amber`, `Amber Corporate`
 , `Crimson`, `Crimson Corporate`
 , `Intel Data 10`
@@ -91,17 +248,27 @@ clientclass, clienttype, services, ActiveCount, contractcount, period, clientcod
 , `Dedicated Custom`, `Dedicated Plus`
 , `Hotels/Channels`, `Hotels/Decoder`
 , `VPN`
-from rcbill_my.clientstats
+
+from rcbill_my.clientstats a
+inner join 
+rcbill_my.rep_custconsolidated b
+on a.clientcode=b.clientcode
+
 where 
 (`Performance`>0 or `Performance Plus`>0 or `Amber`>0 or `Amber Corporate`>0) 
 and 
 (`Extravagance`>0 or `Extravagance Corporate`>0)
 ;
 
-
+##################################
 -- Number of customers on Starter & Value + Any TV Package
+select 'customers on Starter & Value + Any TV Package' as message;
 select 
-clientclass, clienttype, services, ActiveCount, contractcount, period, clientcode, clientname, region, network
+a.clientclass, a.clienttype, a.services, a.ActiveCount, a.contractcount, a.period, a.clientcode, a.clientname, a.region, a.network
+, b.TotalPaymentAmount2021, b.AvgMonthlyPayment2021
+, b.TotalPaymentAmount2020, b.AvgMonthlyPayment2020
+, b.TotalPaymentAmount2019, b.AvgMonthlyPayment2019
+, b.TotalPaymentAmount2018, b.AvgMonthlyPayment2018
 , `Amber`, `Amber Corporate`
 , `Crimson`, `Crimson Corporate`
 , `Intel Data 10`
@@ -121,7 +288,12 @@ clientclass, clienttype, services, ActiveCount, contractcount, period, clientcod
 , `Dedicated Custom`, `Dedicated Plus`
 , `Hotels/Channels`, `Hotels/Decoder`
 , `VPN`
-from rcbill_my.clientstats
+
+from rcbill_my.clientstats a
+inner join 
+rcbill_my.rep_custconsolidated b
+on a.clientcode=b.clientcode
+
 where 
 (`Starter`>0 or `Value`>0) 
 and 
