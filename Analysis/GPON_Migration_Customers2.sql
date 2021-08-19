c0d7707ea6268504c8d7f3f475b58fdb30be9992
rahulw-iv <rahulw.iv@gmail.com>
2021-08-19 16:49:11 +0400

    raw

--- Analysis/GPON_Migration_Customers2.sql
+++ Analysis/GPON_Migration_Customers2.sql
@@ -9,7 +9,18 @@ group by 1,2,3
 order by 1, 4
 ;
 
+
+
+#### first contract date for each network
 select clientcode, network, min(firstcontractdate), max(lastcontractdate)
+from rcbill_my.customercontractsnapshot
+group by 1,2
+order by 1, 3
+;
+
+
+#### first contract date for each servicecategory
+select clientcode, servicecategory, min(firstcontractdate), max(lastcontractdate)
 from rcbill_my.customercontractsnapshot
 group by 1,2
 order by 1, 3
