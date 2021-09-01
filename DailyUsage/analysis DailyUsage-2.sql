4c3debbabcf6515eb053b490b4fda371259dddb0
bisuser <intelvision.sc@outlook.com>
2021-09-01 09:52:25 +0400

    data-bis

--- DailyUsage/analysis DailyUsage-2.sql
+++ DailyUsage/analysis DailyUsage-2.sql
@@ -13,6 +13,12 @@ group by 1
 
 select * from sandvine_usage limit 100;
 
+select * from rcbill.clientcontractdevices where length(mac)>0; -- limit 100;
+select * from rcbill_my.rep_clientcontractdevices where length(mac)>0; -- limit 100;
+
+select * from sandvine_usage where subscriber='00:02:71:cd:0c:89';
+select * from sandvine_usage where upper(subscriber)='00:02:71:cd:10:71';
+
 
 select date(timestamp) as u_date, signature_service_category, count( distinct subscriber) as u_client, count(*) 
 from sandvine_usage
@@ -24,7 +30,12 @@ from sandvine_usage
 group by 1,2,3
 ;
 
-select date(timestamp) as u_date, service_plan, count( distinct subscriber) as u_client, count(*) 
+select timestamp as u_date, service_plan, count( distinct subscriber) as u_client, count(*) 
+from sandvine_usage
+group by 1,2
+;
+
+select timestamp as u_date, location, count( distinct subscriber) as u_client, count(*) 
 from sandvine_usage
 group by 1,2
 ;
