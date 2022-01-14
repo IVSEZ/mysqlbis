8ad81fc557ef925a558f1ca32cb41f44bc06fe7e
rahulw-iv <rahulw.iv@gmail.com>
2022-01-14 09:54:59 +0400

    raw

---
+++ Analysis/parcel_notpresent.sql
@@ -0,0 +1,3 @@
+select reportdate, count(*) as cust_count from rcbill_my.rep_custextract where ONE_YEAR='ONE YEAR' and PARCEL_PRESENT='NOT PRESENT' group by reportdate;
+
+select *, clientcode, clientname, clientclass, IsAccountActive, accountactivitystage from rcbill_my.rep_custextract where ONE_YEAR='ONE YEAR' and PARCEL_PRESENT='NOT PRESENT';
