827bc948511ebed37e029e36b044af04f12496c6
bisuser <intelvision.sc@outlook.com>
2022-01-19 08:19:23 +0400

    data-bis

--- _DailyScript/_setdates.sql
+++ _DailyScript/_setdates.sql
@@ -10,28 +10,28 @@ use rcbill;
 set @lastdate = (select max(month_all_date) from rcbill_my.month_all_date);
 
 #SET DATE
-SET @REPORTDATE=str_to_date('2022-01-17','%Y-%m-%d');
+SET @REPORTDATE=str_to_date('2022-01-18','%Y-%m-%d');
 
 
-SET @rundate='2022-01-17';
+SET @rundate='2022-01-18';
 
 SET @COLNAME1='CLIENTDEBT_REPORTDATE';
 
-set @periodstart='2022-01-17';
-set @periodend='2022-01-17';	
+set @periodstart='2022-01-18';
+set @periodend='2022-01-18';	
 
 
-SET @rundate='2022-01-17';
+SET @rundate='2022-01-18';
 
 #FOR Daily calls script
-SET @date1='2022-01-17';
-SET @date2='2022-01-18';  ### to be one day later
+SET @date1='2022-01-18';
+SET @date2='2022-01-19';  ### to be one day later
 
 
 ## FOR BUDGET ACTUAL ANALYSIS SCRIPT
 ## change the @m# date for the relevant date
 set @revenue=1;
-set @m1='2022-01-17';
+set @m1='2022-01-18';
 
 set @m2='2022-02-28';
 set @m3='2022-03-31';
